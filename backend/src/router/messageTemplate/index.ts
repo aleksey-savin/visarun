7c9351813177c4d8b88218803a370621184933a3
Resonare <47446377+Resonare@users.noreply.github.com>
2025-07-10 12:05:10 +1000

    microfix for prod build

--- backend/src/router/messageTemplate/index.ts
+++ backend/src/router/messageTemplate/index.ts
@@ -2,7 +2,7 @@ import { createMessageTemplateTrpcRoute } from './create.js';
 import { getAllMessageTemplatesTrpcRoute } from './getAll.js';
 import { editMessageTemplateTrpcRoute } from './edit.js';
 import { getOneMessageTemplateTrpcRoute } from './getOne.js';
-import { deleteMessageTemplateTrpcRoute } from './delete.ts';
+import { deleteMessageTemplateTrpcRoute } from './delete.js';
 
 export const messageTemplateRoute = {
   getAll: getAllMessageTemplatesTrpcRoute,
--- webapp/src/pages/MessageTemplates/create.tsx
+++ webapp/src/pages/MessageTemplates/create.tsx
@@ -13,7 +13,6 @@ import {
   FormMessage,
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
-import { Textarea } from '@/components/ui/textarea';
 
 import { SquarePen, MessageSquareDiff } from 'lucide-react';
 import { trpc } from '@/lib/trpcProvider';
