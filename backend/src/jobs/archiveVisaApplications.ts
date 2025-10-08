e08ca76672f3e7fe3a5c66d64eeac14992f9cb51
Aleksey Savin <aleksey.savin@protonmail.com>
2025-10-08 15:22:19 +1000

    fixed scripts

--- backend/src/jobs/archiveVisaApplications.ts
+++ backend/src/jobs/archiveVisaApplications.ts
@@ -14,7 +14,7 @@ let cronJob: cron.ScheduledTask | null = null;
 export const startArchiveVisaApplicationsJob = () => {
   // Run daily at 07:00 UTC: '0 7 * * *'
   cronJob = cron.schedule(
-    '22 5 * * *',
+    '30 5 * * *',
     async () => {
       // Prevent overlapping job executions
       if (isJobRunning) {
